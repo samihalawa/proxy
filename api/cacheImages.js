fdc90e93610192a4187ede9652b33d651d9a2338
samihalawa <samihalawaster@gmail.com>
2024-07-11 23:08:25 +0200

    Update cacheImages.js

--- api/cacheImages.js
+++ api/cacheImages.js
@@ -1,4 +1,10 @@
 const fetch = require('node-fetch');
+const { createClient } = require('@vercel/postgres');
+const { put } = require('@vercel/blob');
+
+const postgresUrl = process.env.POSTGRES_URL;
+const postgresClient = createClient({ connectionString: postgresUrl });
+const blobToken = process.env.BLOB_READ_WRITE_TOKEN;
 
 module.exports = async (req, res) => {
   const { urls } = req.query;
@@ -15,8 +21,23 @@ module.exports = async (req, res) => {
       if (response.ok) {
         const contentType = response.headers.get('content-type');
         if (contentType && contentType.startsWith('image/')) {
-          // Push the URL to the results if it is an image
-          results.push(url);
+          const buffer = await response.buffer();
+          const fileExtension = contentType.split('/')[1];
+          const fileName = `image-${url.split('/').pop()}.${fileExtension}`;
+          const blobResponse = await put(fileName, buffer, {
+            access: 'public',
+            token: blobToken
+          });
+
+          const blobUrl = blobResponse.url;
+          const prompt = url.split('/').pop();
+
+          await postgresClient.sql`
+            INSERT INTO images (url, blob_url, prompt)
+            VALUES (${url}, ${blobUrl}, ${prompt})
+          `;
+
+          results.push({ url, blobUrl, prompt });
         } else {
           console.error(`URL is not an image: ${url}`);
         }
