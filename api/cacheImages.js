62cac67d797bb8066df29013c002e2b273b984fa
samihalawa <samihalawaster@gmail.com>
2024-07-11 23:37:46 +0200

    Update cacheImages.js

--- api/cacheImages.js
+++ api/cacheImages.js
@@ -1,11 +1,6 @@
 const fetch = require('node-fetch');
-const { createClient } = require('@supabase/supabase-js');
 const { Client } = require('pg');
 const { put } = require('@vercel/blob');
-
-const supabaseUrl = 'https://wqhyfotqobtrtxsjmkmr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxaHlmb3Rxb2J0cnR4c2pta21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwMTcyMjIsImV4cCI6MjAzMjU5MzIyMn0.g23QzTWZG5f2UPbXys_1LOqI0tsAvqOOSO5M4XeRfYQ';
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 const pgClient = new Client({
   connectionString: process.env.POSTGRES_URL,
@@ -30,21 +25,19 @@ module.exports = async (req, res) => {
         if (contentType && contentType.startsWith('image/')) {
           const buffer = await response.buffer();
           const fileExtension = contentType.split('/')[1];
-          const prompt = url.split('/').pop().split('.')[0];
-          const fileName = `image-${prompt}.${fileExtension}`;
-          const blobResponse = await put(fileName, buffer, {
+          const fileName = `image-${url.split('/').pop().split('.')[0]}.${fileExtension}`;
+          const blob = await put(fileName, buffer, {
             access: 'public',
             token: process.env.BLOB_READ_WRITE_TOKEN
           });
 
-          const blobUrl = blobResponse.url;
-
+          const blobUrl = blob.url;
           await pgClient.query(
-            'INSERT INTO images (url, blob_url, prompt) VALUES ($1, $2, $3)',
-            [url, blobUrl, prompt]
+            'INSERT INTO images (url, blob_url) VALUES ($1, $2)',
+            [url, blobUrl]
           );
 
-          results.push({ url, blobUrl, prompt });
+          results.push({ url, blobUrl });
         } else {
           console.error(`URL is not an image: ${url}`);
         }
@@ -56,5 +49,5 @@ module.exports = async (req, res) => {
     }
   }
 
-  return res.status(200).json({ cachedUrls: results });
+  res.status(200).json({ cachedUrls: results });
 };
